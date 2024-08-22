dc86e24f7b3949dd8fa62a6ab4ddfc45e5cbf157
Murad Biashimov <murad.biashimov@aiven.io>
2024-08-22 10:54:45 +0200

    feat: improve errors

--- error_test.go
+++ error_test.go
@@ -2,10 +2,14 @@
 package aiven
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"net/http"
 	"os"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -100,6 +104,96 @@ func TestIsAlreadyExists(t *testing.T) {
 	for _, opt := range cases {
 		t.Run(opt.name, func(t *testing.T) {
 			assert.Equal(t, opt.expect, IsAlreadyExists(opt.err))
+		})
+	}
+}
+
+func TestFromResponse(t *testing.T) {
+	cases := []struct {
+		name           string
+		operationID    string
+		statusCode     int
+		body           []byte
+		expectErr      error
+		expectStringer string
+		expectBytes    []byte
+	}{
+		{
+			name:        "404",
+			operationID: "UserAuth",
+			statusCode:  http.StatusNotFound,
+			body: []byte(`{
+			  "message": "Account does not exist",
+			  "errors": [
+				{
+				  "error_code": "account_not_found",
+				  "message": "Account does not exist",
+				  "status": 404
+				}
+			  ]
+			}`),
+			expectStringer: `[404 UserAuth]: Account does not exist: [{"error_code":"account_not_found","message":"Account does not exist","status":404}]`,
+			expectBytes:    nil,
+			expectErr: Error{
+				Message: "Account does not exist",
+				Errors: []any{map[string]any{
+					"error_code": "account_not_found",
+					"message":    "Account does not exist",
+					"status":     float64(404),
+				}},
+				OperationID: "UserAuth",
+				Status:      http.StatusNotFound,
+			},
+		},
+		{
+			name:        "200",
+			operationID: "UserAuth",
+			statusCode:  http.StatusOK,
+			body:        []byte(`{"success": True}`),
+			expectBytes: []byte(`{"success": True}`),
+			expectErr:   nil,
+		},
+		{
+			name:           "Invalid json",
+			operationID:    "UserAuth",
+			statusCode:     http.StatusInternalServerError,
+			body:           []byte(`unknown error`),
+			expectBytes:    nil,
+			expectStringer: `[500 UserAuth]: unknown error: null`,
+			expectErr: Error{
+				OperationID: "UserAuth",
+				Message:     "unknown error",
+				Status:      http.StatusInternalServerError,
+			},
+		},
+		{
+			name:           "Valid json, Error fields type mismatch",
+			operationID:    "UserAuth",
+			statusCode:     http.StatusBadRequest,
+			body:           []byte(`{"message": {"key": "value"}}`), // message is not string
+			expectStringer: `[400 UserAuth]: json: cannot unmarshal object into Go struct field Error.message of type string: null`,
+			expectBytes:    nil,
+			expectErr: Error{
+				OperationID: "UserAuth",
+				Message:     "json: cannot unmarshal object into Go struct field Error.message of type string",
+				Status:      http.StatusBadRequest,
+			},
+		},
+	}
+
+	for _, opt := range cases {
+		t.Run(opt.name, func(t *testing.T) {
+			rsp := &http.Response{
+				StatusCode: opt.statusCode,
+				Body:       io.NopCloser(bytes.NewReader(opt.body)),
+			}
+
+			b, err := fromResponse(opt.operationID, rsp)
+			assert.Equal(t, opt.expectBytes, b)
+			assert.Empty(t, cmp.Diff(opt.expectErr, err))
+			if err != nil {
+				assert.Equal(t, opt.expectStringer, err.Error())
+			}
 		})
 	}
 }
