b1d4e2797d08a67d634e6a4942c8b138aa736f79
Timo Riski <timo.riski@aiven.io>
2024-11-27 10:33:03 +0200

    feat: move AlloyDB Omni APIs to separate module

--- handler/service/service.go
+++ handler/service/service.go
@@ -11,21 +11,6 @@ import (
 )
 
 type Handler interface {
-	// AlloyDbOmniGoogleCloudPrivateKeyIdentify get Google service account key
-	// GET /v1/project/{project}/service/{service_name}/alloydbomni/google_cloud_private_key
-	// https://api.aiven.io/doc/#tag/Service:_AlloyDB_Omni/operation/AlloyDbOmniGoogleCloudPrivateKeyIdentify
-	AlloyDbOmniGoogleCloudPrivateKeyIdentify(ctx context.Context, project string, serviceName string) (*AlloyDbOmniGoogleCloudPrivateKeyIdentifyOut, error)
-
-	// AlloyDbOmniGoogleCloudPrivateKeyRemove delete Google service account key
-	// DELETE /v1/project/{project}/service/{service_name}/alloydbomni/google_cloud_private_key
-	// https://api.aiven.io/doc/#tag/Service:_AlloyDB_Omni/operation/AlloyDbOmniGoogleCloudPrivateKeyRemove
-	AlloyDbOmniGoogleCloudPrivateKeyRemove(ctx context.Context, project string, serviceName string) (*AlloyDbOmniGoogleCloudPrivateKeyRemoveOut, error)
-
-	// AlloyDbOmniGoogleCloudPrivateKeySet add Google service account key
-	// POST /v1/project/{project}/service/{service_name}/alloydbomni/google_cloud_private_key
-	// https://api.aiven.io/doc/#tag/Service:_AlloyDB_Omni/operation/AlloyDbOmniGoogleCloudPrivateKeySet
-	AlloyDbOmniGoogleCloudPrivateKeySet(ctx context.Context, project string, serviceName string, in *AlloyDbOmniGoogleCloudPrivateKeySetIn) (*AlloyDbOmniGoogleCloudPrivateKeySetOut, error)
-
 	// ListProjectServiceTypes list service types for a project
 	// GET /v1/project/{project}/service_types
 	// https://api.aiven.io/doc/#tag/Service/operation/ListProjectServiceTypes
@@ -275,45 +260,6 @@ type ServiceHandler struct {
 	doer doer
 }
 
-func (h *ServiceHandler) AlloyDbOmniGoogleCloudPrivateKeyIdentify(ctx context.Context, project string, serviceName string) (*AlloyDbOmniGoogleCloudPrivateKeyIdentifyOut, error) {
-	path := fmt.Sprintf("/v1/project/%s/service/%s/alloydbomni/google_cloud_private_key", url.PathEscape(project), url.PathEscape(serviceName))
-	b, err := h.doer.Do(ctx, "AlloyDbOmniGoogleCloudPrivateKeyIdentify", "GET", path, nil)
-	if err != nil {
-		return nil, err
-	}
-	out := new(AlloyDbOmniGoogleCloudPrivateKeyIdentifyOut)
-	err = json.Unmarshal(b, out)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-func (h *ServiceHandler) AlloyDbOmniGoogleCloudPrivateKeyRemove(ctx context.Context, project string, serviceName string) (*AlloyDbOmniGoogleCloudPrivateKeyRemoveOut, error) {
-	path := fmt.Sprintf("/v1/project/%s/service/%s/alloydbomni/google_cloud_private_key", url.PathEscape(project), url.PathEscape(serviceName))
-	b, err := h.doer.Do(ctx, "AlloyDbOmniGoogleCloudPrivateKeyRemove", "DELETE", path, nil)
-	if err != nil {
-		return nil, err
-	}
-	out := new(AlloyDbOmniGoogleCloudPrivateKeyRemoveOut)
-	err = json.Unmarshal(b, out)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-func (h *ServiceHandler) AlloyDbOmniGoogleCloudPrivateKeySet(ctx context.Context, project string, serviceName string, in *AlloyDbOmniGoogleCloudPrivateKeySetIn) (*AlloyDbOmniGoogleCloudPrivateKeySetOut, error) {
-	path := fmt.Sprintf("/v1/project/%s/service/%s/alloydbomni/google_cloud_private_key", url.PathEscape(project), url.PathEscape(serviceName))
-	b, err := h.doer.Do(ctx, "AlloyDbOmniGoogleCloudPrivateKeySet", "POST", path, in)
-	if err != nil {
-		return nil, err
-	}
-	out := new(AlloyDbOmniGoogleCloudPrivateKeySetOut)
-	err = json.Unmarshal(b, out)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
 func (h *ServiceHandler) ListProjectServiceTypes(ctx context.Context, project string) (*ListProjectServiceTypesOut, error) {
 	path := fmt.Sprintf("/v1/project/%s/service_types", url.PathEscape(project))
 	b, err := h.doer.Do(ctx, "ListProjectServiceTypes", "GET", path, nil)
@@ -940,29 +886,6 @@ type AlertOut struct {
 	ServiceName *string   `json:"service_name,omitempty"` // Service name
 	ServiceType *string   `json:"service_type,omitempty"` // Service type code
 	Severity    string    `json:"severity"`               // Severity of the event
-}
-
-// AlloyDbOmniGoogleCloudPrivateKeyIdentifyOut AlloyDbOmniGoogleCloudPrivateKeyIdentifyResponse
-type AlloyDbOmniGoogleCloudPrivateKeyIdentifyOut struct {
-	ClientEmail  string `json:"client_email"`   // Email address of Google service account key
-	PrivateKeyId string `json:"private_key_id"` // Google service account key ID
-}
-
-// AlloyDbOmniGoogleCloudPrivateKeyRemoveOut AlloyDbOmniGoogleCloudPrivateKeyRemoveResponse
-type AlloyDbOmniGoogleCloudPrivateKeyRemoveOut struct {
-	ClientEmail  string `json:"client_email"`   // Email address of Google service account key
-	PrivateKeyId string `json:"private_key_id"` // Google service account key ID
-}
-
-// AlloyDbOmniGoogleCloudPrivateKeySetIn AlloyDbOmniGoogleCloudPrivateKeySetRequestBody
-type AlloyDbOmniGoogleCloudPrivateKeySetIn struct {
-	PrivateKey string `json:"private_key"` // Google Service Account Credentials
-}
-
-// AlloyDbOmniGoogleCloudPrivateKeySetOut AlloyDbOmniGoogleCloudPrivateKeySetResponse
-type AlloyDbOmniGoogleCloudPrivateKeySetOut struct {
-	ClientEmail  string `json:"client_email"`   // Email address of Google service account key
-	PrivateKeyId string `json:"private_key_id"` // Google service account key ID
 }
 
 // AlloydbomniOut Service type information
