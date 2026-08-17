f833d2912558f8ed1a9e35a2282d8eca25aad2c7
GitHub <noreply@github.com>
2026-08-17 03:16:48 +0000

    chore(update): bump openapi schema (2026-08-17)

--- handler/user/user.go
+++ handler/user/user.go
@@ -616,6 +616,7 @@ const (
 	ProjectMembershipTypeProjectAiGatewayKeysRead       ProjectMembershipType = "project:ai_gateway_keys:read"
 	ProjectMembershipTypeProjectAiGatewayKeysWrite      ProjectMembershipType = "project:ai_gateway_keys:write"
 	ProjectMembershipTypeProjectAuditLogsRead           ProjectMembershipType = "project:audit_logs:read"
+	ProjectMembershipTypeProjectEventLogsRead           ProjectMembershipType = "project:event_logs:read"
 	ProjectMembershipTypeProjectIntegrationsRead        ProjectMembershipType = "project:integrations:read"
 	ProjectMembershipTypeProjectIntegrationsWrite       ProjectMembershipType = "project:integrations:write"
 	ProjectMembershipTypeProjectNetworkingRead          ProjectMembershipType = "project:networking:read"
@@ -626,6 +627,7 @@ const (
 	ProjectMembershipTypeReadOnly                       ProjectMembershipType = "read_only"
 	ProjectMembershipTypeRoleOrganizationAdmin          ProjectMembershipType = "role:organization:admin"
 	ProjectMembershipTypeRoleProjectAdmin               ProjectMembershipType = "role:project:admin"
+	ProjectMembershipTypeRoleProjectRead                ProjectMembershipType = "role:project:read"
 	ProjectMembershipTypeRoleServicesMaintenance        ProjectMembershipType = "role:services:maintenance"
 	ProjectMembershipTypeRoleServicesRecover            ProjectMembershipType = "role:services:recover"
 	ProjectMembershipTypeServiceConfigurationWrite      ProjectMembershipType = "service:configuration:write"
@@ -637,7 +639,7 @@ const (
 )
 
 func ProjectMembershipTypeChoices() []string {
-	return []string{"admin", "developer", "operator", "organization:app_users:write", "organization:billing:read", "organization:billing:write", "organization:domains:write", "organization:event_logs:read", "organization:groups:write", "organization:networking:read", "organization:networking:write", "organization:projects:write", "organization:sustainability:read", "organization:users:write", "project:ai_gateway_keys:read", "project:ai_gateway_keys:write", "project:audit_logs:read", "project:integrations:read", "project:integrations:write", "project:networking:read", "project:networking:write", "project:permissions:read", "project:services:read", "project:services:write", "read_only", "role:organization:admin", "role:project:admin", "role:services:maintenance", "role:services:recover", "service:configuration:write", "service:data:write", "service:logs:read", "service:metrics:read", "service:secrets:read", "service:users:write"}
+	return []string{"admin", "developer", "operator", "organization:app_users:write", "organization:billing:read", "organization:billing:write", "organization:domains:write", "organization:event_logs:read", "organization:groups:write", "organization:networking:read", "organization:networking:write", "organization:projects:write", "organization:sustainability:read", "organization:users:write", "project:ai_gateway_keys:read", "project:ai_gateway_keys:write", "project:audit_logs:read", "project:event_logs:read", "project:integrations:read", "project:integrations:write", "project:networking:read", "project:networking:write", "project:permissions:read", "project:services:read", "project:services:write", "read_only", "role:organization:admin", "role:project:admin", "role:project:read", "role:services:maintenance", "role:services:recover", "service:configuration:write", "service:data:write", "service:logs:read", "service:metrics:read", "service:secrets:read", "service:users:write"}
 }
 
 type TokenOut struct {
